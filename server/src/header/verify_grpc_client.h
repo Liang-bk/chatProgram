4fe40042bb5bfc4c31c07406d5ffe56bf0ecb494
Liang-bk <Kxq0309@outlook.com>
2025-06-12 20:42:40 +0800

    use grpc, add config(need to be changed to json config)

---
+++ server/src/header/verify_grpc_client.h
@@ -0,0 +1,35 @@
+//
+// Created by 86183 on 2025/6/12.
+//
+
+#ifndef VARIRFY_GRPC_CLIENT_H
+#define VARIRFY_GRPC_CLIENT_H
+#pragma once
+
+#include <grpcpp/grpcpp.h>
+#include "message.grpc.pb.h"
+#include "constant.h"
+#include "singleton.h"
+
+using grpc::Channel;
+using grpc::Status;
+using grpc::ClientContext;
+
+using message::GetVerifyRequest;
+using message::GetVerifyResponse;
+using message::VerifyService;
+
+
+class VerifyGrpcClient : public Singleton<VerifyGrpcClient> {
+    friend class Singleton<VerifyGrpcClient>;
+public:
+    ~VerifyGrpcClient();
+    GetVerifyResponse GetVarifyCode(std::string email);
+private:
+    VerifyGrpcClient();
+    std::unique_ptr<VerifyService::Stub> stub_;
+};
+
+
+
+#endif //VARIRFY_GRPC_CLIENT_H
